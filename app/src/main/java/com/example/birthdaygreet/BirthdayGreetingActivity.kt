038b876816a63f01c83806392ed9bf56d5f9f5b5
Piyush <war847342@gmail.com>
2022-07-13 22:51:59 +0530

    Birthday Greet App V 1.0 Date : 13-07-2022

---
+++ app/src/main/java/com/example/birthdaygreet/BirthdayGreetingActivity.kt
@@ -0,0 +1,23 @@
+package com.example.birthdaygreet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    companion object{
+        const val NAME_EXTRA = "name_extra"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+
+
+        val name = intent.getStringExtra(NAME_EXTRA)
+        val textView = findViewById<TextView>(R.id.birthdayGreeting).apply {
+            text = name
+        }
+
+    }
+}
